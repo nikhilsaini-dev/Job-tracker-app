af651af16eef401bf1ef7c96ef7aae1a584074bc
Nikhil saini <nikhilsaini8108@gmail.com>
2026-03-31 14:09:46 +0530

    updated

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
 
     setLoading(true);
     try {
-      const res = await fetch("https://job-tracker-app-wrwz.onrender.com/api/auth/login", {
+      const res = await fetch("https://job-tracker-app-wrwz.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
